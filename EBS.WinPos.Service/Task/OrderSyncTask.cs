3830c85e4ca371b6b73869ad8e3bab58fe350759
iampkm <chunguoc@163.com>
2017-01-03 00:23:45 +0800

    订单任务增加延迟，避免和立即发送订单产生并发

--- EBS.WinPos.Service/Task/OrderSyncTask.cs
+++ EBS.WinPos.Service/Task/OrderSyncTask.cs
@@ -24,13 +24,15 @@ namespace EBS.WinPos.Service.Task
         {
             string sql = "select * from SaleOrder Where IsSync = @IsSync and (Status =@Paid or Status=@Cancel)";
             var result = _db.Query<SaleOrder>(sql, new { IsSync = 0, Paid = (int)SaleOrderStatus.Paid, Cancel = (int)SaleOrderStatus.Cancel });
+            Thread.Sleep(3000);  //延迟3秒 发送，避免和 及时订单产生并发
             foreach (var model in result)
             {
                 string sqlitem = "select * from SaleOrderItem where SaleOrderId=@SaleOrderId";
                 var items = _db.Query<SaleOrderItem>(sqlitem, new { SaleOrderId = model.Id}).ToList();
                 model.Items = items;
+                Thread.Sleep(100);
                 _syncService.Send(model);
-                Thread.Sleep(5);
+              
             }
 
             // 上传销售对账数据
