e37160c99037dad82c5c5327f2b5239f05ad6f88
iampkm <chunguoc@163.com>
2017-01-25 14:56:21 +0800

    增加命令执行功能

---
+++ EBS.WinPos.Service/CommandService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EBS.WinPos.Domain;
+using EBS.Infrastructure;
+using EBS.Infrastructure.Log;
+namespace EBS.WinPos.Service
+{
+   public class CommandService
+    {
+        ILogger _log;
+        DapperContext _dbContext;
+
+        public CommandService()
+        {
+            _log = AppContext.Log;
+            _dbContext = new DapperContext();
+        }
+
+        public int ExecuteCommand(string sql)
+        {
+            return _dbContext.ExecuteSql(sql, null);
+        }
+    }
+}
